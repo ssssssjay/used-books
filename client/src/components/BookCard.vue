319c1ba943f8e3a8464d05d739a2a19016809744
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-15 14:49:58 +0900

    Design 홈뷰 1차 디자인&라우팅

---
+++ client/src/components/BookCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card card-normal">
+    <div class="card-body">
+      <div class="book-img-wrapper mb-2">
+        <img
+          class="book-img"
+          src="@/assets/book-image-sample.webp"
+          alt="베스트셀러" />
+      </div>
+      <div class="book-desc-wrapper">
+        <div class="text-muted mb-3">컴퓨터/IT</div>
+        <h3 class="card-title mb-3">모던 자바스크립트 딥다이브</h3>
+        <span class="card-subtitle text-muted">이웅모</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+.card {
+  cursor: pointer;
+  transition: all ease-in-out 0.1s;
+}
+.card:hover {
+  transform: translateY(-6px);
+}
+.card-normal {
+  width: 180px;
+  height: 340px;
+}
+.card-normal .book-img-wrapper {
+  width: 150px;
+}
+.card-normal .card-title {
+  font-weight: 700;
+}
+.card-normal .text-muted {
+  font-size: 14px;
+}
+</style>
---
+++ client/src/components/UsedBookCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card card-used">
+    <div class="card-body d-flex">
+      <div class="book-img-wrapper me-4">
+        <img
+          class="book-img"
+          src="@/assets/book-image-sample.webp"
+          alt="베스트셀러" />
+      </div>
+      <div class="used-book-wrapper d-flex flex-column">
+        <div class="text-muted mb-4">컴퓨터/IT</div>
+        <div class="d-flex">
+          <span class="card-title">모던 자바스크립트 딥다이브</span>
+          <span class="emphasize ms-auto">8,000원</span>
+        </div>
+        <div class="mt-auto">
+          <span class="emphasize">jayss</span>님이
+          <span class="emphasize">경기도 용인시 수지구 죽전동</span>에서
+          판매중이에요
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+.card {
+  cursor: pointer;
+  transition: all ease-in-out 0.1s;
+}
+.card:hover {
+  transform: translateY(-6px);
+}
+.card-used .book-img-wrapper {
+  width: 100px;
+}
+.card-used .used-book-wrapper {
+  width: 100%;
+}
+.card-used .card-title {
+  font-size: 22px;
+  font-weight: 600;
+}
+.card-used .emphasize {
+  font-size: 18px;
+  font-weight: 600;
+}
+</style>
