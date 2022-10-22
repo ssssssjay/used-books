e4baf9f7601a0b180bb468aa7c4b7a17322fa772
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-22 14:36:55 +0900

    feat: 알라딘 api사용 라우팅 처리 및 검색기능 추가

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ require("dotenv").config({ path: `mysql/.env` });
 const multer = require("multer");
 const path = require("path");
 const axios = require("axios");
-
 const mysql = require("./mysql");
 
 app.use(
@@ -26,15 +25,11 @@ let sess = {
   },
   userId: "",
 };
-
 app.use(session(sess));
 const corsOptions = {
   origin: "http://localhost:5173", // 허용할 도메인 설정
   optionsSuccessStatus: 200,
 };
-
-// 뷰(프론트) => 노드서버로 요청을하면 => 노드서버에서 api요청 코드를 짜서, 노드서버에서 응답을받고 우리 프론트로 쏴준다
-// 유즈라우트써서 => 홈뷰가아니다? 이즈홈뷰같은 반응형데이터를 바꿔서 저 인풋창을 헤데에다 올리는?
 
 app.use(cors(corsOptions));
 // 이미지 업로드
@@ -78,6 +73,10 @@ app.post(
     res.send(fileInfo);
   }
 );
+
+const bookRoute = require("./routes/book");
+
+app.use("/book", bookRoute);
 
 app.listen(3000, () => {
   console.log("서버가 포트 3000번으로 시작되었습니다.");
---
+++ server/routes/book.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/best", async (req, res) => {
+  try {
+    const categoryId = req.query.categoryId;
+    const result = await axios
+      .get("http://www.aladin.co.kr/ttb/api/ItemList.aspx", {
+        params: {
+          ttbkey: "ttbksj26190321932001",
+          QueryType: "Bestseller",
+          MaxResults: 10,
+          start: 1,
+          SearchTarget: "Book",
+          output: "js",
+          Version: 20131101,
+          Cover: "Big",
+          // 주석처리하고 요청보내면 0으로 갑니다
+          CategoryId: categoryId,
+        },
+      })
+      .then((res) => res.data);
+
+    // if (result.errorCode) {
+    // throw new Error(result.errorMessage);
+    // }
+    // { errorCode: 4, errorMessage: 'API출력이 금지된 회원입니다.' }
+    console.log("success");
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+});
+
+router.get("/search", async (req, res) => {
+  try {
+    // http://localhost:3000/book/search?q=자바스크립트
+    console.log(req.path); // /search
+    console.log(req.query); // { q: '자바스크립트' }
+    console.log(req.params); // { }
+    const searchQuery = req.query.q;
+    const result = await axios
+      .get("http://www.aladin.co.kr/ttb/api/ItemSearch.aspx", {
+        params: {
+          ttbkey: "ttbksj26190321932001",
+          Query: searchQuery,
+          QueryType: "Keyword",
+          MaxResults: 5,
+          start: 1,
+          SearchTarget: "Book",
+          output: "js",
+          Version: 20131101,
+        },
+      })
+      .then((res) => res.data);
+
+    console.log("success");
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+});
+
+module.exports = router;
+// result의 일부
+// status: 200,
+// statusText: 'OK',
+// headers: AxiosHeaders {
+//   'cache-control': 'private',
+//   'content-type': 'application/json; charset=utf-8',
+//   server: 'Microsoft-IIS/8.5',
+//   'x-aspnet-version': '2.0.50727',
+//   'set-cookie': 'AladdinUS=ZKrK%2boUzjBg%3d&USA=0; domain=aladin.co.kr; expires=Sun, 19-Oct-2042 03:56:55 GMT; path=/',
+//   'x-powered-by': 'ASP.NET',
+//   p3p: "CP='CAO PSA CONi OTR OUR DEM ONL'",
+//   date: 'Wed, 19 Oct 2022 03:56:54 GMT',
+//   connection: 'close',
+//   'content-length': '12122',
+//   [Symbol(defaults)]: null
+// },
+
+// http://localhost:3000/book/best/jay?test=test
+// console.log(req.path); // /best/jay
+// console.log(req.query); // { test: 'test' }
+// console.log(req.params); // { user: 'jay' }
