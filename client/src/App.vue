434f9fdabca2b382793932b6d4144899b3487b0a
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-22 14:39:41 +0900

    feat: 헤더 레이아웃 생성

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <HeaderLayout></HeaderLayout>
     <RouterView></RouterView>
   </div>
 </template>
 
 <script lang="ts">
+import HeaderLayout from "./components/layouts/HeaderLayout.vue";
 export default {
   setup() {
     return {};
   },
+  components: { HeaderLayout },
 };
 </script>
 
---
+++ client/src/components/layouts/HeaderLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="py-3 border-bottom shadow-sm sticky-top bg-white">
+    <nav class="container">
+      <ul class="d-flex flex-wrap align-items-center">
+        <li class="me-auto">
+          <RouterLink to="/" class="fs-5 fw-bold"
+            ><span class="text-success">used:</span> Books</RouterLink
+          >
+        </li>
+        <li class="me-4">
+          <RouterLink
+            to="/library"
+            class="fs-6 fw-bold"
+            :class="{ active: route.path === '/library' }"
+            >library</RouterLink
+          >
+        </li>
+        <li class="me-4">
+          <RouterLink
+            to="/chat"
+            class="fs-6 fw-bold"
+            :class="{ active: route.path === '/chat' }"
+            >chatting</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/history"
+            class="fs-6 fw-bold"
+            :class="{ active: route.path === '/history' }"
+            >history</RouterLink
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+const route = useRoute();
+console.log(route.path);
+</script>
+
+<style scoped>
+.active {
+  color: #457e2b;
+}
+</style>
