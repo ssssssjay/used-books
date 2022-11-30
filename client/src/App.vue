78426632263c8cd4e5b0bb59f72be82d24f5683d
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-30 13:51:21 +0900

    feat(usedBookCreate): 중고도서등록 페이지 이동 기능 추가
    - 전체 서비스에서 접근 가능한 버튼 컴포넌트 생성 및 라우팅 추가

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,17 +2,26 @@
   <div>
     <HeaderLayout></HeaderLayout>
     <RouterView></RouterView>
+    <UsedBookCreateBtn class="btn-sticky-create"></UsedBookCreateBtn>
   </div>
 </template>
 
 <script lang="ts">
 import HeaderLayout from "./components/layouts/HeaderLayout.vue";
+import UsedBookCreateBtn from "./components/UsedBookCreateBtn.vue";
+
 export default {
   setup() {
     return {};
   },
-  components: { HeaderLayout },
+  components: { HeaderLayout, UsedBookCreateBtn },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.btn-sticky-create {
+  position: fixed;
+  bottom: 10%;
+  right: 5%;
+}
+</style>
---
+++ client/src/components/UsedBookCreateBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="btn btn-success" @click="goUsedBookCreatePage">
+      중고등록
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goUsedBookCreatePage = () => {
+  router.push("/used-book/create");
+};
+</script>
+
+<style scoped></style>
