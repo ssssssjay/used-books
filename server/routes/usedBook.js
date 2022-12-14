99e27a7d35c1c889cc1faf5b2c3781eed29afb2f
Sungjae.Kwak <sungjae8970@gmail.com>
2022-12-14 19:06:45 +0900

    feat(usedBook.js): id에 의거한 중고데이터 가져오기
    - 요청에서 쿼리로 받는 id값으로 테이블에서 정보를 가져옴

--- server/routes/usedBook.js
+++ server/routes/usedBook.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const axios = require("axios");
 const mysql = require("../mysql");
 
-// TODO: product_id에 의거한 불러오기
 router.get("/", async (req, res) => {
   try {
-  } catch (error) {}
+    const productId = req.query.id; // ?=2
+    const result = await mysql.query("getUsedBook", [productId]);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 router.post("/create", async (req, res) => {
   try {
     const result = await mysql.query("insertUsedBook", [req.body.param]);
-    console.log(result);
     res.send(result);
   } catch (error) {
     console.log(error);
