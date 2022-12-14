5bc7604d0119c222efdb0d40c0664e619f28b0c0
Sungjae.Kwak <sungjae8970@gmail.com>
2022-12-14 19:08:42 +0900

    feat(sql): 쿼리문 2개 추가
    - 중고도서터 get (id값에 의거)
    - 유저데이터 get (email값에 의거)

--- server/mysql/sql.js
+++ server/mysql/sql.js
@@ -4,5 +4,7 @@ module.exports = {
   userDetail: `select * from user where user_id=?`,
   userUpdate: `update user set ? where user_id=?`,
   userDelete: `delete from user where user_id=?`,
+  getUser: `select * from user where user_email=?`,
+  getUsedBook: `select * from product where product_id=?`,
   insertUsedBook: `insert into product set ?`,
 };
