8beeec53a6d061a47e1cfebcb9f5ca77bb0470ec
Sungjae.Kwak <sungjae8970@gmail.com>
2023-02-06 03:08:16 +0900

    fix(router, store): delete console, fix persistedstate

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -51,9 +51,9 @@ const router = createRouter({
   ],
 });
 router.beforeEach((to, from, next) => {
-  console.log("to", to);
-  console.log("from", from);
-  console.log(store.state.userInfo);
+  // console.log("to", to);
+  // console.log("from", from);
+  // console.log(store.state.userInfo);
   console.log(store.state.isLogin);
   if (
     store.state.isLogin == true ||
