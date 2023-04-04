95b77df11b4d7cc049e7e63cedaf9fac14b411fc
Sungjae.Kwak <sungjae8970@gmail.com>
2023-04-04 16:03:44 +0900

    style: font-Noto Sans KR

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import mixins from "@/mixins/index";
+// import mixins from "@/mixins/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "@/styles/reset.css";
+import "@/styles/main.css";
 import store from "@/store";
 
 import App from "./App.vue";
@@ -15,7 +16,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(store);
-app.mixin(mixins);
+// app.mixin(mixins);
 app.mount("#app");
 
 window.Kakao.init("4b6c5749640870e5658414bdbfd3d03c");
